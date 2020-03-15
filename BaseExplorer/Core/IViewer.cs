afcce7e8e02fd6002f763a9b3ad6e909104f7914
AlphaBs <ksi123456ab@naver.com>
2020-03-15 15:54:02 +0900

    add IViewer

---
+++ BaseExplorer/Core/IViewer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseExplorer.Core
+{
+    interface IViewer
+    {
+        string GetEncodedName(string decodedName, bool isdir);
+        string GetDecodedName(string encodedName, bool isdir);
+
+        bool CheckIsEncodedName(string filename);
+        bool CheckIsEncodedName(string filename, bool isdir);
+
+        void EncodeFile(string parent, string name);
+        void EncodeFile(string filepath);
+        void DecodeFile(string parent, string name);
+        void DecodeFile(string filepath);
+
+        void EncodeDir(string parent, string name);
+        void EncodeDir(string dirpath);
+        void DecodeDir(string parent, string name);
+        void DecodeDir(string dirpath);
+    }
+}
